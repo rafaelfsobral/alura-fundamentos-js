493c4c431b532469fccd55ca2622c2966354fd23
rafaelfsobral <85068949+rafaelfsobral@users.noreply.github.com>
2021-09-10 09:48:00 -0300

    Delete template-strings.js

--- template-strings.js
+++
@@ -1,9 +0,0 @@
-const nome = "Rafael"
-const idade = 2021-1986;
-const cidadeDeNascimento = "Rio de Janeiro";
-
-// const apresentacao = "Meu nome é " + nome + ", minha idade é " + idade + " e nasci na cidade de " + cidadeDeNascimento;
-
-const apresentacao = `Meu nome é ${nome}, minha idade é ${idade} e nasci na cidade de ${cidadeDeNascimento}.`
-
-console.log(apresentacao);
